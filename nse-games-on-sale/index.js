b43a75813edd8acaaf009fb7115220bc5babb46c
Luiz Marcelo Martins Franco <luiz.franco@fpf.br>
2019-01-22 15:06:26 -0400

    Updating code for v2

--- nse-games-on-sale/index.js
+++ nse-games-on-sale/index.js
@@ -1,33 +1,31 @@
-const SwitchEshop = require('nintendo-switch-eshop');
+const { getGamesAmerica, getPrices, parseNSUID, Region } = require("nintendo-switch-eshop");
 const fs = require('fs');
 
-console.log("Fetching all EU games...");
-SwitchEshop.getGamesEurope().then(games => {
-    const gamesWithIds = games.filter(g => g.nsuid_txt);
-    const ids = gamesWithIds.map(g => g.nsuid_txt[0]);
+(async () => {
+    console.log("Fetching all games...");
+    const games = await getGamesAmerica();
+    const gameIds = games.map(g => parseNSUID(g, Region.AMERICA));
 
-    console.log(`Fetching prices for ${ids.length} games on 'Great Britain'...`);
-    SwitchEshop.getPrices("GB", ids).then(priceData => {
-        const gamesOnSale = [];
+    console.log(`Fetching prices for ${gameIds.length} games on 'Canada'...`);
+    const priceData = await getPrices("CA", gameIds);
+    const gamesOnSale = [];
 
-        // Filter games with discount
-        const discountPrices = priceData.prices.filter(p => p.discount_price);
+    // Filter games with discount
+    const discountPrices = priceData.prices.filter(p => p.discount_price);
 
-        // Filter games on our discount list
-        discountPrices.forEach(price => {
-            const game = gamesWithIds.find(g => g.nsuid_txt[0] == price.title_id);
-            if(game) {
-                game.price_data = price;
-                gamesOnSale.push(game);
-            }
-        });
+    // Filter games on our discount list
+    discountPrices.forEach(price => {
+        const game = games.find(g => parseNSUID(g, Region.AMERICA) == price.title_id);
+        if (game) {
+            game.price_data = price;
+            gamesOnSale.push(game);
+        }
+    });
 
-        console.log(`Found ${gamesOnSale.length} games on sale`);
+    console.log(`Found ${gamesOnSale.length} games on sale`);
 
-        const jsonData = JSON.stringify(gamesOnSale, null, 4);
-        fs.writeFileSync("./gamedata.json", jsonData, "utf8");
+    const jsonData = JSON.stringify(gamesOnSale, null, 4);
+    fs.writeFileSync("./gamedata.json", jsonData, "utf8");
 
-        console.log("Saved results to gamedata.json");
-    })
-
-});
+    console.log("Saved results to gamedata.json");
+})();
